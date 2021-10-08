9a75eb804711de18122a0dcff722f1c9771efe96
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-10-08 19:05:16 -0400

    Removed

--- login/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Login from "./Login";
-
-ReactDom.render(<Login />, document.getElementById("app"));
--- navbar/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import Navbar from "./Navbar";
-
-ReactDom.render(<Navbar />, document.getElementById("app"));
