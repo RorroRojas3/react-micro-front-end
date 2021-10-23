61393caf7120d7c4ab4175b9c2b5f4ea52a1115e
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-10-23 12:31:54 -0400

    Updated boilterplate

---
+++ shell/webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html",
+});
+module.exports = {
+  mode: "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    hashFunction: "sha256",
+  },
+  plugins: [htmlPlugin],
+};
